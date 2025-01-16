48a5337614c6104d302466a534a3ec496fbc1918
shivam <sharmshivam959@gmail.com>
2025-01-16 18:38:11 +0530

    9

---
+++ src/firstjava/nestedif.java
@@ -0,0 +1,29 @@
+package firstjava;
+
+import java.util.Scanner;
+
+public class nestedif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int num1;
+ System.out.println("maximum:");
+ Scanner Sc=new Scanner("system.in");
+ 
+ num1=Sc.nextInt();
+ if (num1>=45)
+ {
+	 System.out.println("my name: shivam");
+ }
+ else
+ {
+	 System.out.println("sorry wrong input");
+ }
+	}
+}
+ 
+ 
+
+	
+
+
